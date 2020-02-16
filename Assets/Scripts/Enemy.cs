9f307883c37720616d4e4b46fa279182e0d5aa1c
loChris <44450518+loChris@users.noreply.github.com>
2020-02-16 13:44:40 -0500

    Enemy now 'spawns' out of view and glides downwards. If he reaches the bottom, he respwans at the top with a random x value.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 8f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move down at 4 meters per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        //if bottom of screen, respawn at top with a new random x position
+        if (transform.position.y <= -8)
+        {
+            transform.position = new Vector3(Random.Range(-9f, 9f), 8f, 0);
+        }
+    }
+}
