60afd4dcc790f3860fd0b1491747ae3e954b48c7
loChris <44450518+loChris@users.noreply.github.com>
2020-02-17 13:57:08 -0500

    added enemy spawner coroutine

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,7 +10,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 8f, 0);
+        transform.position = new Vector3(Random.Range(-9f, 9f), 8f, 0);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private float _spawnTimer = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-9f, 9f), 8f, 0);
+            Instantiate(_enemyPrefab, spawnPos, Quaternion.identity);
+            yield return new WaitForSeconds(_spawnTimer);
+        }
     }
 }
