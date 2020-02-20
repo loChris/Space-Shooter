8a3ec21aea7adae68c3dc019a5f30ca66fbdb5b8
loChris <44450518+loChris@users.noreply.github.com>
2020-02-20 15:19:13 -0500

    powerups spawn at random locations and times between 3-8 seconds

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _powerupPrefab;
     [SerializeField] private GameObject _enemyContainer;
+    [SerializeField] private GameObject _powerupContainer;
     [SerializeField] private float _spawnTimer = 5f;
     private bool _playerAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
     // Update is called once per frame
@@ -20,7 +23,19 @@ public class Spawner : MonoBehaviour
     {
     }
 
-    IEnumerator SpawnRoutine()
+    IEnumerator SpawnPowerupRoutine()
+    {
+        //every 3 - 7 seconds spawn a powerup
+        while (_playerAlive == true)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-9f, 9f), 8f, 0);
+            GameObject newPowerup = Instantiate(_powerupPrefab, spawnPos, Quaternion.identity);
+            newPowerup.transform.parent = _powerupContainer.transform;
+            yield return new WaitForSeconds(Random.Range(3f, 8f));
+        }
+    }
+
+    IEnumerator SpawnEnemyRoutine()
     {
         while (_playerAlive == true)
         {
