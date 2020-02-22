750866c5cc6e311e835c608c5581bfef97966bdb
loChris <44450518+loChris@users.noreply.github.com>
2020-02-22 15:42:03 -0500

    added game over screen for when player dies

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,12 +9,15 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Text _scoreText;
     [SerializeField] private Image _livesImg;
     [SerializeField] private Sprite[] _livesSprites;
+    [SerializeField] private Text _gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         //assign text component to handle to use it
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
+
     }
 
     public void UpdateScoreOnScreen(int playerScore)
@@ -25,5 +28,9 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _livesImg.sprite = _livesSprites[currentLives];
+        if (currentLives <= 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+        }
     }
 }
